4b53ae031b577aa6d4534c3dfa1a19dd55d30587
Brandon Little <brlittle86@gmail.com>
2017-10-17 08:16:25 -0700

    added Controller for Characters, added migration for table data

---
+++ TableTopCharacterManager/TableTopCharacterManager/Data/TableTopCharacterManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TableTopCharacterManager.Models
+{
+    public class TableTopCharacterManagerContext : DbContext
+    {
+        public TableTopCharacterManagerContext (DbContextOptions<TableTopCharacterManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TableTopCharacterManager.Models.Character> Character { get; set; }
+    }
+}
---
+++ TableTopCharacterManager/TableTopCharacterManager/Migrations/20171017151439_Initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TableTopCharacterManager.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CharacterClass = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterRace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Equipment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gold = table.Column<int>(type: "int", nullable: false),
+                    IsAlive = table.Column<bool>(type: "bit", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    PhysicalDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Character");
+        }
+    }
+}
