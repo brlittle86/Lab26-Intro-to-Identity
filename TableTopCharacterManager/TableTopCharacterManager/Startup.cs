378428baf086d4d9f5ee928f7b99759612832dbb
Brandon Little <brlittle86@gmail.com>
2017-10-17 08:26:41 -0700

    added the db context for the user and the identity framework to the app services

--- TableTopCharacterManager/TableTopCharacterManager/Startup.cs
+++ TableTopCharacterManager/TableTopCharacterManager/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TableTopCharacterManager.Data;
 using TableTopCharacterManager.Models;
 
 namespace TableTopCharacterManager
@@ -28,6 +26,13 @@ namespace TableTopCharacterManager
 
             services.AddDbContext<TableTopCharacterManagerContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("TableTopCharacterManagerContext")));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TableTopCharacterManagerContext")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
