aef2cf083e458020979a325e3ddf753467d7ed8e
Brandon Little <brlittle86@gmail.com>
2017-10-17 08:22:01 -0700

    created applicationdbcontext and applicationuser model

---
+++ TableTopCharacterManager/TableTopCharacterManager/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace TableTopCharacterManager.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime Birthday { get; set; }
+    }
+}
